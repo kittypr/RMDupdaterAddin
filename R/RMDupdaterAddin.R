2d0dcada4be54161f9c15c80978a195fa2ac8560
Julia <julettazhuk@gmail.com>
2018-08-17 13:02:38 +0700

    Refactored, choice for updating added, functions separated

--- R/RMDupdaterAddin.R
+++ R/RMDupdaterAddin.R
@@ -6,16 +6,32 @@ library(rstudioapi)
 library(googledrive)
 library(knitr)
 
+
 find <- function(patern, original){
-  pLength <- length(patern)
-  oLength <- length(original)
-  candidate <- seq.int(length=oLength-pLength+1)
-  for (i in seq.int(length=pLength)) {
+  pattern.length <- length(patern)
+  original.length <- length(original)
+  candidate <- seq.int(length=original.length-pattern.length+1)
+  for (i in seq.int(length=pattern.length)) {
     candidate <- candidate[patern[i] == original[candidate + i - 1]]
   }
   candidate
 }
 
+upload <- function(odt.report, report.name, report.name.draft, sync.path){
+  result <- googledrive::drive_upload(odt.report, name = report.name, type = "document")
+  fair <- result[[2]] # gdoc id for clean copy
+  result <- googledrive::drive_upload(odt.report, name = report.name.draft, type = "document")
+  draft <- result[[2]] # gdoc id for draft
+
+  fair.link <- paste0(" https://docs.google.com/document/d/", fair, "/")
+  fair.string <- paste0("# ", report.name, fair.link)
+  draft.string <- paste0("gdrive update ", draft, " ", odt.report, " --name ", report.name.draft)
+  cat("\n", fair.string, draft.string,file=sync.path,sep="\n",append=TRUE)
+}
+
+compare <- function(echo.md.path, draft.id){
+  res <- shell(paste0("RMDupdater.py ", echo.md.path, " ", draft.id), intern = TRUE) # getting answer from python
+}
 
 RMDupdaterAddin <- function() {
 
@@ -46,82 +62,65 @@ RMDupdaterAddin <- function() {
     original <- NaN
     iter <- 1
 
+
     shiny::observeEvent(input$upd, {
-# temporary, testing some functions
+      # temporary, testing some functions
       # extracting report name
       current.report <- rstudioapi::getActiveDocumentContext()
       report.path <- current.report$path
 
-      title.string <- file.new$contents[1] # CHANGE BEFORE RELEASE to 2
-      title <- strsplit(title.string, split = '"', fixed = TRUE)
+      title.string <- current.report$contents[1] # CHANGE BEFORE RELEASE to 2
+      title <- strsplit(title.string, split = "\"", fixed = TRUE)
       report.name <- title[[c(1,2)]]
       report.name.draft <- paste0("\"", report.name, ". Draft\"")
-      report.name <- paste0("\"",report.name, "\"")
+      report.name <- paste0("\"", report.name, "\"")
 
       project.path <- rstudioapi::getActiveProject() # path for sync_reports
       sync.path <- paste0(project.path, "/sync_reports.sh")
       sync.info <- readLines(sync.path)
+
+      # looking for report info in sync_reports.sh
       regular.exp <- paste0("^# ", report.name)
       result <- grep(regular.exp, sync.info)
+
+      # building path to odt, TODO: add manual selection if not exists
       odt.report <- gsub(".R", ".odt", report.path, fixed=TRUE) # CHANGE BEFORE RELEASE to .rmd
-      if (length(result) == 0){
-        choice <- menu(c("Yes"), title = "Report wasn`t found in sync_reports.sh. Do you want create new finish and draft?")
+
+      if (length(result) == 0){ # report info wasnt found
+        print("Files were not found in sync_reports.sh.")
+        choice <- menu(c("Yes"), title = "Do you want create new fair and draft?")
         if (choice == 1){
-          result <- googledrive::drive_upload(odt.report, name = report.name, type = "document")
-          print(result)
-          finish <- result[[2]]
-          result <- googledrive::drive_upload(odt.report, name = report.name.draft, type = "document")
-          draft <- result[[2]]
-          finish.link <- paste0(" https://docs.google.com/document/d/", finish, "/")
-          finish.string <- paste0("# ", report.name, finish.link)
-          draft.string <- paste0("gdrive update ", draft, " ", odt.report, " --name ", report.name.draft)
-          print(finish.string)
-          print(draft.string)
-          cat("\n", finish.string, draft.string,file=sync.path,sep="\n",append=TRUE)
+          upload(odt.report, report.name, report.name.draft, sync.path)
         }
+        print("Uploaded successfully")
+        shiny::stopApp()
       }
       else {
-        # python magic there
         draft.string <- sync.info[result[1] + 1]
         draft.id <- strsplit(draft.string, split = " ", fixed = TRUE)[[c(1,3)]]
-        print(draft.id)
-        googledrive::drive_update(googledrive::as_id(draft.id), odt.report)
+        print("Draft info found. Comparison process . . .")
+        # KNITTING ECHO MD HERE
+        # PATH TO ECHO MD HERE
+        #compare(report.path, draft.id)
+        choice <- menu(c("Yes"), title = "Do you want update draft?")
+        if (choice == 1){
+          googledrive::drive_update(googledrive::as_id(draft.id), odt.report)
+        }
+        print("Updated successfully")
+        shiny::stopApp()
       }
-
-
-
-#      file <- rstudioapi::selectFile(caption = "Select File", label = "Select", path = NULL,
-#                                     filter = "*.changes", existing = TRUE)
-#      print(file)
-#      result <- googledrive::drive_upload(file, name = "experimental")
-#      str(result[[2]]) # gdoc id
-#      file.new <- rstudioapi::getActiveDocumentContext()
-#      title.string <- file.new$contents[1]
-#      print(title.string)
-#      str(file.new$path) # rmd path
-#      title <- strsplit(title.string, split = '"', fixed = TRUE)
-#      print(title.string)
-#      print(title[[c(1,2)]]) # report name
-#      print("hi")
-#      res <- shell("RMDupdater.py C:\\Users\\jetbrains\\Documents\\research-scripts-master\\research-scripts-master\\RSCH\\rsch_1369_deveco18_report\\reports\\full_report\\full_report.md 1wg3wTu2tv1gs_eCoUFZUkSNyyBVaoCeTYOIKZjGSKZU", intern = TRUE)
-#      print(res) #  answer
-    })
-
-    shiny::observeEvent(input$done, {
-      # Return the brushed points. See ?shiny::brushedPoints.
-      shiny::stopApp()
     })
 
     shiny::observeEvent(input$chc, {
       myChanges <<- readLines(rstudioapi::selectFile(caption = "Select File", label = "Select", path = NULL,
-                                                    filter = "*.changes", existing = TRUE))
+                                                     filter = "*.changes", existing = TRUE))
       context <<- rstudioapi::getActiveDocumentContext()
       original <<- context$contents
     })
 
     shiny::observeEvent(input$prv, {
-# temporary unavailable
-# TODO: just do it
+      # temporary unavailable
+      # TODO: just do it
       shiny::stopApp()
     })
 
@@ -178,6 +177,11 @@ RMDupdaterAddin <- function() {
         iter <<- iter + 1
       }
     })
+
+    shiny::observeEvent(input$done, {
+      shiny::stopApp()
+    })
+
   }
 
   viewer <- shiny::paneViewer()
@@ -185,3 +189,5 @@ RMDupdaterAddin <- function() {
 }
 
 RMDupdaterAddin()
+
+
