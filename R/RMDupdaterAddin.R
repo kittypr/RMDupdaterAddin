a36492407370a008105e2cccd5906be79264769d
Julia <julettazhuk@gmail.com>
2018-08-20 16:43:44 +0700

    refactored, interface changed, python calling added

--- R/RMDupdaterAddin.R
+++ R/RMDupdaterAddin.R
@@ -30,7 +30,7 @@ Upload <- function(odt.report, report.name, report.name.draft, sync.path){
 }
 
 Compare <- function(echo.md.path, draft.id){
-  res <- shell(paste0("RMDupdater.py ", echo.md.path, " ", draft.id), intern = TRUE) # getting answer from python
+  answer <- shell(paste0("RMDupdater.py ", echo.md.path, " ", draft.id), intern = TRUE) # getting answer from python
 }
 
 PerformRefactor <- function(contents, from, to, useWordBoundaries = FALSE) {
@@ -54,17 +54,22 @@ PerformRefactor <- function(contents, from, to, useWordBoundaries = FALSE) {
 }
 
 Echo <- function(content, context){
+  # copying content of current report and replace all ECHO=FALSE to ECHO=TRUE, return  changed content
   file.create("report_copy.rmd")
-  spec <- PerformRefactor(content, from = "Developer", to = "Developter")
-  transformed <- paste(spec$refactored, collapse = "\n")
+  spec <- PerformRefactor(content, from = "knitr::opts_chunk$set(echo = FALSE)", to = "knitr::opts_chunk$set(echo = TRUE)") # CHANGE BEFORE RELIASE
+  spec$refactored  # return as character vector
+  #  transformed <- paste(spec$refactored, collapse = "\n")  # return as string witn \n
 }
 
-CopyAndCompare <- function(echo.true.report){
+CopyAndCompare <- function(echo.true.report, draft.id){
   file.create("report_copy.rmd")
-  cat(echo.true.report, file = "report_copy.rmd")
+  out <- file(description="report_copy.rmd", open="w", encoding="UTF-8")
+  writeLines(echo.true.report, con=out)
+  close(con=out)
   knitr::knit(input = "report_copy.rmd", output = "echo_report.md")
-  #pythonmagic there
+  answer <- Compare(echo.md.path = "echo_report.md", draft.id = draft.id)
   file.remove(c("report_copy.rmd", "echo_report.md"))
+  answer
 }
 
 RMDupdaterAddin <- function() {
@@ -72,11 +77,7 @@ RMDupdaterAddin <- function() {
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("RMD update"),
     miniUI::miniButtonBlock(
-      shiny::actionButton("upd", "Update", icon = shiny::icon("arrow-top")),
-      border = "bottom"
-    ),
-    miniUI::miniButtonBlock(
-      shiny::actionButton("chc", "Check out", icon = shiny::icon("arrow-bottom")),
+      shiny::actionButton("upd", "Update", icon = shiny::icon("backward")),
       border = "bottom"
     ),
     miniUI::miniButtonBlock(
@@ -86,6 +87,10 @@ RMDupdaterAddin <- function() {
     miniUI::miniButtonBlock(
       shiny::actionButton("prv", "Find prev", icon = shiny::icon("arrow-left")),
       border = "bottom"
+    ),
+    miniUI::miniButtonBlock(
+      shiny::actionButton("fupd", "Force update", icon = shiny::icon("fast-backward")),
+      border = "top"
     )
   )
 
@@ -96,45 +101,106 @@ RMDupdaterAddin <- function() {
     original <- NaN
     iter <- 1
 
+    draft.id <- NaN
+    current.report <- NaN
+    report.path <- NaN
+    report.name <- NaN
+    report.name.draft <- NaN
+    result <- NaN
+    odt.report <- NaN
+    sync.path <- NaN
+    sync.info <- NaN
 
-    shiny::observeEvent(input$upd, {
-      # temporary, testing some functions
+    GetIformation <- function(){
       # extracting report name
-      current.report <- rstudioapi::getActiveDocumentContext()
-      report.path <- current.report$path
+      current.report <<- rstudioapi::getActiveDocumentContext()
+      report.path <<- current.report$path
 
-      title.string <- current.report$contents[1] # CHANGE BEFORE RELEASE to 2
+      title.string <- current.report$contents[2]  # CHANGE BEFORE RELEASE to 2
       title <- strsplit(title.string, split = "\"", fixed = TRUE)
-      report.name <- title[[c(1,2)]]
-      report.name.draft <- paste0("\"", report.name, ". Draft\"")
-      report.name <- paste0("\"", report.name, "\"")
+      report.name <<- title[[c(1,2)]]
+      report.name.draft <<- paste0("\"", report.name, ". Draft\"")
+      report.name <<- paste0("\"", report.name, "\"")
 
       project.path <- rstudioapi::getActiveProject() # path for sync_reports
-      sync.path <- paste0(project.path, "/sync_reports.sh")
-      sync.info <- readLines(sync.path)
+      sync.path <<- paste0(project.path, "/sync_reports.sh")
+      sync.info <<- readLines(sync.path)
 
       # looking for report info in sync_reports.sh
       regular.exp <- paste0("^# ", report.name)
-      result <- grep(regular.exp, sync.info)
+      result <<- grep(regular.exp, sync.info)
 
-      # building path to odt, TODO: add manual selection if not exists
-      odt.report <- gsub(".R", ".odt", report.path, fixed=TRUE) # CHANGE BEFORE RELEASE to .rmd
+      # building path to odt
+      odt.report <<- gsub(".rmd$", ".odt", report.path) # CHANGE BEFORE RELEASE to .rmd
+      if ( ! file.exists(odt.report)){
+        print(paste0("File ", odt.report, " was not found."))
+        if (menu(c("Yes"), title = "Select it manually?") == 1){
+          odt.report <<- rstudioapi::selectFile(caption = "Select knitted report:", label = "Select", path = NULL,
+                                             filter = "*.odt", existing = TRUE)
+        }
+        else {
+          shiny::stopApp()
+        }
+      }
+    }
 
-      if (length(result) == 0){ # report info wasnt found
-        print("Files were not found in sync_reports.sh.")
+
+    shiny::observeEvent(input$upd, {
+      GetIformation()
+      if (length(result) == 0){  # report info wasnt found
+        print(paste0("Information associated with ", report.name, " was not found in sync_reports.sh."))
         choice <- menu(c("Yes"), title = "Do you want create new fair and draft?")
         if (choice == 1){
           Upload(odt.report, report.name, report.name.draft, sync.path)
+          print("Uploaded successfully")
         }
-        print("Uploaded successfully")
         shiny::stopApp()
       }
       else {
         draft.string <- sync.info[result[1] + 1]
-        draft.id <- strsplit(draft.string, split = " ", fixed = TRUE)[[c(1,3)]]
+        draft.id <<- strsplit(draft.string, split = " ", fixed = TRUE)[[c(1,3)]]
         print("Draft info found. Comparison process . . .")
         echo.true.report <- Echo(content = current.report$contents, current.report)
-        CopyAndCompare(echo.true.report)
+        answer <- CopyAndCompare(echo.true.report, draft.id)
+        if (answer[1] == "OUTDATED BLOCKS FOUNDED"){
+          print("Changes detected. Please use 'Find next' button to see outdated blocks.")
+          print("You can ignore changes and use 'Force update' button.")
+        }
+        else if (answer[1] == "UP TO DATE"){
+          print("RMDupdater didn't detect any changes.")
+          choice <- menu(c("Yes"), title = "Do you want update draft?")
+          if (choice == 1){
+            googledrive::drive_update(googledrive::as_id(draft.id), odt.report)
+            print("Updated successfully")
+          }
+          shiny::stopApp()
+        }
+        else {
+          print("Some errors occurred:")
+          print(answer)
+        }
+      }
+    })
+
+    shiny::observeEvent(input$fupd, {
+      if (is.nan(draft.id)){
+        GetIformation()
+        if (length(result) == 0){  # report info wasnt found
+          print("Files were not found in sync_reports.sh.")
+          print("Use 'Update' button.")
+        }
+        else {
+          draft.string <- sync.info[result[1] + 1]
+          draft.id <<- strsplit(draft.string, split = " ", fixed = TRUE)[[c(1,3)]]
+          choice <- menu(c("Yes"), title = "Do you want update draft?")
+          if (choice == 1){
+            googledrive::drive_update(googledrive::as_id(draft.id), odt.report)
+            print("Updated successfully")
+          }
+          shiny::stopApp()
+        }
+      }
+      else {
         choice <- menu(c("Yes"), title = "Do you want update draft?")
         if (choice == 1){
           googledrive::drive_update(googledrive::as_id(draft.id), odt.report)
@@ -144,13 +210,6 @@ RMDupdaterAddin <- function() {
       }
     })
 
-    shiny::observeEvent(input$chc, {
-      myChanges <<- readLines(rstudioapi::selectFile(caption = "Select File", label = "Select", path = NULL,
-                                                     filter = "*.changes", existing = TRUE))
-      context <<- rstudioapi::getActiveDocumentContext()
-      original <<- context$contents
-    })
-
     shiny::observeEvent(input$prv, {
       # temporary unavailable
       # TODO: just do it
@@ -158,8 +217,17 @@ RMDupdaterAddin <- function() {
     })
 
     shiny::observeEvent(input$nxt, {
+      if (iter == 1){
+        log.file <- "log.changes"
+        if ( ! file.exists("log.changes")){
+          message("Use 'Highlight' firstly!")
+        }
+        myChanges <<- readLines(log.file)
+        context <<- rstudioapi::getActiveDocumentContext()
+        original <<- context$contents
+      }
       if (length(myChanges) == 1){
-        print("FILE WAS NOT CHOSEN")
+        print("NO CHANGES DETECTED")
       }
       else if (iter > length(myChanges)){
         print("END OF CHANGES FILE. USE 'FIND PREV' OR 'DONE'")
@@ -221,7 +289,7 @@ RMDupdaterAddin <- function() {
   shiny::runGadget(ui, server)
 }
 
-RMDupdaterAddin()
+#RMDupdaterAddin()
 
 
 
